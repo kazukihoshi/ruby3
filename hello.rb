7934843cbfa9dccbc262c1f60884c051c93940fc
kazukihoshi <starrysky.blue30@gmail.com>
2023-02-17 13:53:15 +0000

    [add]4章

--- hello.rb
+++ hello.rb
@@ -12,3 +12,37 @@ puts "I" + "am" + "Sam"
 puts "Samの年齢は" + 27.to_s + "です"
 
 puts 100 + "200".to_i
+
+webcamp = "プログラミング学習"
+puts webcamp
+
+webcamp = "オンラインプログラミング学習"
+puts webcamp
+
+Pi = 3.14
+puts Pi
+
+Pi = 100
+puts Pi
+
+puts 100
+puts 100 + 3
+puts 100-3
+puts 100*3
+puts 100/3
+puts 100 % 3
+
+name = "A"
+weight = 50
+puts name + "さんの体重は" + weight.to_s + "kgです"
+puts "#{name}さんの体重は#{weight}kgです"
+puts "#{name}さんの体重は#{weight*2}kgです"
+
+names = ["Git" , "HTML", "CSS"]
+puts names[1]
+
+tall = {"太郎" => 185, "二郎" => 170, "花子" => 150}
+puts tall["太郎"]
+
+subjects = ["国語", "算数", "理科", "社会"]
+puts subjects[2]
